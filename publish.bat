e3a6769f0f4c285ffae9895a885a53076ba36e82
grankko <grankko@gmail.com>
2017-11-10 16:45:36 +0100

    published tried deleting non-existing file

--- publish.bat
+++ publish.bat
@@ -3,6 +3,5 @@ set OutputPath=%1
 set SelectedEnvironment=%2
 dotnet publish CostOMeter.Web\CostOMeter.Web.csproj -c release -o %OutputPath%
 copy EnvironmentConfigs\web.%SelectedEnvironment%.config %OutputPath%\web.config
-del %OutputPath%\web.production.config
 del %OutputPath%\*.pdb
 rd %OutputPath%\wwwroot\src /s /q
