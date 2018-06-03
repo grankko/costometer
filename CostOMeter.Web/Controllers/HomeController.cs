132bec95920cd5d2a766197ac2745c718d70fe08
grankko <grankko@gmail.com>
2018-06-03 23:28:42 +0200

    webpack dependancy in packages

--- CostOMeter.Web/Controllers/HomeController.cs
+++ CostOMeter.Web/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace CostOMeter.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string listName)
         {
             return View();
         }
